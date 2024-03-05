85fd8476c479f0960efe73f32a4cbad66d755c17
Frank <101807917+frankischilling@users.noreply.github.com>
2024-03-05 16:58:14 -0500

    Update dmenu_command.sh

--- shell/dmenu_command.sh
+++ shell/dmenu_command.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 # dmenu_command.sh
 
-# Run dmenu to get the user's command
-cmd=$(dmenu -p "Run command:" <&-)
+while true; do
+    # Run dmenu to get the user's command
+    cmd=$(dmenu -p "Run command:" <&-)
 
-# Check if the user entered a command
-if [ -n "$cmd" ]; then
-    # Run the command in st
-    st -e sh -c "$cmd; exec zsh"
-fi
+    # Check if the user canceled dmenu
+    if [ $? -ne 0 ]; then
+        break
+    fi
+
+    # Check if the command is not empty
+    if [ -n "$cmd" ]; then
+        # Check if the command is valid
+        if command -v "$cmd" >/dev/null 2>&1; then
+            # Run the command in st
+            st -e sh -c "$cmd; exec zsh"
+            break  # Exit the loop if a valid command is provided
+        else
+            # Display an error message for invalid command
+            echo "Invalid command: $cmd" | dmenu -p "Error" -l 2
+        fi
+    fi
+done
